5696e3cdb77737e4ad46b334568b3aa5cca4065c
Andy Jost <ajost@synopsys.com>
2018-03-25 14:44:57 -0700

    Adds getICurryForModule, which calls curry2json as needed.

---
+++ src/python/getsource.py
@@ -0,0 +1,98 @@
+import inspect
+import os
+import subprocess
+import sys
+
+def findfiles(searchpaths, names):
+  '''
+  Searches the specified paths for a file with the given name and suffix.
+
+  Parameters:
+  -----------
+  ``searchpaths``
+      A sequence of paths to search.
+  ``names``
+      A sequence of file names to search for.
+
+  Returns:
+  --------
+  A sequence containing the files found.
+  '''
+  assert not isinstance(searchpaths, str)
+  for path in searchpaths:
+    for name in names:
+      filename = os.path.join(path, name)
+      if os.path.exists(filename):
+        yield filename
+
+def findCurryModule(name, searchpaths):
+  '''
+  Searches for a Curry module.
+
+  Parameters:
+  -----------
+  ``name``
+      The module name.
+  ``searchpaths``
+      A sequence of paths to search (i.e., CURRYPATH split on ':').
+
+  Raises:
+  -------
+  ``ValueError`` if the module is not found.
+
+  Returns:
+  --------
+  The name of either an ICurry-JSON file (suffix: .json) or Curry source file
+  (suffix: .curry)
+  '''
+  # Search for the ICurry-JSON file first, then the .curry file.
+  names = [os.path.join('.curry', name + '.json'), name + '.curry']
+  files = findfiles(searchpaths, names)
+  try:
+    file_ = next(files)
+  except StopIteration:
+    raise ValueError('module "%s" not found' % name)
+  return os.path.abspath(file_)
+
+def curry2json():
+  thispath = inspect.getsourcefile(sys.modules[__name__])
+  curry2json = os.path.abspath(
+      os.path.join(thispath, '../../../../bin/curry2json')
+    )
+  assert os.path.exists(curry2json)
+  return curry2json
+
+def jsonFilename(curryfile):
+  assert curryfile.endswith('.curry')
+  path,name = os.path.split(curryfile)
+  return os.path.join(path, '.curry', name[:-6]+'.json')
+
+def getICurryForModule(name, searchpaths):
+  '''
+  Gets the ICurry source for a module.  May invoke curry2json.
+
+  Parameters:
+  -----------
+  ``name``
+      The module name.
+  ``searchpaths``
+      A sequence of paths to search (i.e., CURRYPATH split on ':').
+
+  Raises:
+  -------
+  ``ValueError`` if the module is not found.
+
+  Returns:
+  The name of the ICurry-JSON file.
+  '''
+  curryfile = findCurryModule(name, searchpaths)
+  if curryfile.endswith('.json'):
+    return curryfile
+  # curryfile is a .curry file.  Call curry2json to produce the ICurry-JSON file.
+  json = jsonFilename(curryfile)
+  assert not os.path.exists(json)
+  sink = open('/dev/null', 'w')
+  retcode = subprocess.call([curry2json(), curryfile], stdout=sink, stderr=sink)
+  if retcode or not os.path.exists(json): #pragma: no cover
+    raise RuntimeError('curry2json "%s" failed' % curryfile)
+  return json
--- tests/test_findcurry.py
+++ tests/test_findcurry.py
@@ -1,6 +1,8 @@
 # Encoding: utf-8
+from curry import getsource
 import cytest
-from curry import findfiles
+import os
+import shutil
 
 class TestFindCurry(cytest.TestCase):
   def test_findFile(self):
@@ -9,49 +11,102 @@ class TestFindCurry(cytest.TestCase):
 
         data/filetree
         ├── a
-        │   └── a.foo
+        │   ├── a.foo
+        │   └── .curry
+        │       └── a.json
         ├── b
         │   ├── a
         │   │   ├── a.curry
         │   │   └── a.foo
         │   └── a.curry
         └── c
+            ├── a
             └── c.curry
     '''
-    ff = findfiles.findfile
-    self.assertEqual(list(ff(['data/filetree/a'], 'a')), [])
+    ff = getsource.findfiles
     self.assertEqual(
-        list(ff(['data/filetree/b'], 'a'))
+        list(ff(
+            names=['a.curry']
+          , searchpaths=['data/filetree/a']
+          ))
+      , []
+      )
+    self.assertEqual(list(ff(['data/filetree/a'], ['a.curry'])), [])
+    self.assertEqual(
+        list(ff(['data/filetree/b'], ['a.curry']))
       , ['data/filetree/b/a.curry']
       )
     self.assertEqual(
-        list(ff(['data/filetree/b/a'], 'a'))
+        list(ff(['data/filetree/b/a'], ['a.curry']))
       , ['data/filetree/b/a/a.curry']
       )
     self.assertEqual(
-        list(ff(['data/filetree/b'], 'a/a'))
+        list(ff(['data/filetree/b'], ['a/a.curry']))
       , ['data/filetree/b/a/a.curry']
       )
     self.assertEqual(
         list(ff(
             ['data/filetree/a', 'data/filetree/b', 'data/filetree/b/a']
-          , 'a'
+          , ['a.curry']
           ))
       , ['data/filetree/b/a.curry', 'data/filetree/b/a/a.curry']
       )
     self.assertEqual(
-        list(ff(['data/filetree/a'], 'a', ['foo']))
+        list(ff(['data/filetree/a'], ['a.foo']))
       , ['data/filetree/a/a.foo']
       )
     self.assertEqual(
         list(ff(
             ['data/filetree/a', 'data/filetree/b/a']
-          , 'a'
-          , ['curry', 'foo']
+          , ['a.curry', 'a.foo']
           ))
       , [   'data/filetree/a/a.foo'
           , 'data/filetree/b/a/a.curry'
           , 'data/filetree/b/a/a.foo'
           ]
+      )
+    self.assertEqual(
+        list(ff(['data/filetree/c'], ['a']))
+      , ['data/filetree/c/a']
+      )
+
+  def test_findCurryModule(self):
+    self.assertEqual(
+        getsource.findCurryModule('c', searchpaths=['data/filetree/c'])
+      , os.path.abspath('data/filetree/c/c.curry')
+      )
+    self.assertEqual(
+        getsource.findCurryModule('a', searchpaths=['data/filetree/b'])
+      , os.path.abspath('data/filetree/b/a.curry')
+      )
+    # Under a/ there is no a.curry, but there is a .curry/a.json file.
+    # It should be found before b/a.curry is located.
+    self.assertEqual(
+        getsource.findCurryModule(
+            'a'
+          , searchpaths=['data/filetree/'+a_or_b for a_or_b in 'ab']
+          )
+      , os.path.abspath('data/filetree/a/.curry/a.json')
+      )
+
+  def test_getICurryForModule(self):
+    self.assertEqual(
+        getsource.getICurryForModule('a', ['data/filetree/a'])
+      , os.path.abspath('data/filetree/a/.curry/a.json')
+      )
+    #
+    jsonfile = 'data/curry/.curry/hello.json'
+    try:
+      os.remove(jsonfile)
+    except OSError:
+      pass
+    self.assertFalse(os.path.exists(jsonfile))
+    self.assertEqual(
+        getsource.getICurryForModule('hello', ['data/curry'])
+      , os.path.abspath(jsonfile)
+      )
+    self.assertEqual(
+        open('data/curry/hello.json.au', 'r').read()
+      , open('data/curry/.curry/hello.json', 'r').read()
       )
 
