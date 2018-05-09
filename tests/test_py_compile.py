1bd8560ad72072eadff79e5d9e8900c245755379
Andy Jost <ajost@synopsys.com>
2018-05-09 09:39:14 -0700

    Removes debug code from test_py_compile.py

--- tests/test_py_compile.py
+++ tests/test_py_compile.py
@@ -3,14 +3,13 @@ import curry
 import unittest
 
 class TestPyCompile(cytest.TestCase):
-  # @unittest.expectedFailure
+  @unittest.expectedFailure
   def testCompileString(self):
     '''Test direct compilation of a string.'''
     text = '''
       fib n | n < 2 = 1
             | True  = (fib (n-1)) + (fib (n-2))
     '''
-    breakpoint()
     fib = curry.compile(text)
     eight = curry.eval(fib.fib(5)).next()
     self.assertEqual(eight, 8)
