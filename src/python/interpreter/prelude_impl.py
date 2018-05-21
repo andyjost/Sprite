fa884652bbf2e5811d09927fcd52cdc9c8182de1
Andy Jost <ajost@synopsys.com>
2018-05-20 18:22:02 -0700

    Moves the implementation of Prelude.apply into prelude_impl.py

---
+++ src/python/interpreter/prelude_impl.py
@@ -0,0 +1,21 @@
+'''
+Implements partial function application.
+'''
+from . import runtime
+
+def apply(interpreter, partapplic, arg):
+  missing, term = partapplic # note: "missing" is unboxed.
+  assert missing >= 1
+  if missing == 1:
+    yield term.info
+    for t in term.successors:
+      yield t
+    yield arg
+  else:
+    yield partapplic.info
+    yield missing-1
+    yield runtime.Node(term.info, *(term.successors+[arg]))
+
+def failed(interpreter):
+  return ['node(%s)' % interpreter.closure['_System.Failure']]
+  
