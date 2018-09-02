e778890fdb9a5419186bcfb750e54c34704144fb
Andy Jost <ajost@synopsys.com>
2018-09-02 08:07:34 -0700

    Slightly changes the name generated for compiled expressions.

--- src/python/interpreter/compile.py
+++ src/python/interpreter/compile.py
@@ -3,6 +3,7 @@ Implements Interpreter.compile.
 '''
 
 from .. import importer
+from .. import icurry
 
 def compile(
     interp, string, mode='module', imports=None, modulename='_interactive_'
@@ -44,16 +45,25 @@ def compile(
       if icur.name.startswith('_') and icur.name in interp.modules:
         del interp.modules[icur.name]
   elif mode == 'expr':
+    # Compile the expression with a legal name but then rename it to "<expr>".
+    # It has to be a legal name during compilation, but renaming makes it clear
+    # this is a system-generated special name.
+    compiled_name = 'compiled_expression'
+    visible_name = '<expr>'
+    compiled_ident = icurry.IName(modulename, compiled_name)
+    visible_ident = icurry.IName(modulename, visible_name)
     stmts, currypath = importer.getImportSpecForExpr(
         interp
       , interp.modules.keys() if imports is None else imports
       )
-    stmts += ['expression = ' + string]
-    string = '\n'.join(stmts)
-    icur = importer.str2icurry(string, currypath)
+    stmts += ['%s = %s' % (compiled_name, string)]
+    curry_code = '\n'.join(stmts)
+    icur = importer.str2icurry(curry_code, currypath)
+    icur.functions[visible_ident] = icur.functions.pop(compiled_ident)
+    icur.functions[visible_ident].ident = visible_ident
     module = interp.import_(icur)
     del interp.modules[icur.name]
-    expr = interp.expr(module.expression)
+    expr = interp.expr(getattr(module, '.symbols')[visible_name])
     interp._step(expr)
     return expr
   else:
