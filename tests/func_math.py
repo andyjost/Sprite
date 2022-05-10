302753827496cb2a1d9ae10cf19fa3dffbaa61d0
Andy Jost <andy.m.jost@gmail.com>
2022-05-10 14:35:29 -0700

    Skipping crashing tests under C++ for func_math.

--- tests/func_math.py
+++ tests/func_math.py
@@ -1,7 +1,9 @@
 '''Math test programs.'''
 import cytest # from ./lib; must be first
+import curry
 
 class TestMath(cytest.FunctionalTestCase):
   SOURCE_DIR = 'data/curry/math/'
-  # RUN_ONLY = ['']
-  # SKIP = ['']
+  # RUN_ONLY = []
+  if curry.flags['backend'] == 'cxx':
+    SKIP = ['floatmath|intmath05[89]|intmath0[67][0-9]']
